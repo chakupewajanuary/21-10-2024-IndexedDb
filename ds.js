a0dac51c29d7609b61c13ef6e1ba5d432bd0232e
chakupewa <chakupewajanuary38@gamil.com>
2024-10-21 16:22:50 +0300

    for the indexedDb concept

---
+++ ds.js
@@ -0,0 +1,34 @@
+
+    // const Dbname = document.getElementById('textDB');
+    // const DbVersion = document.getElementById('textversion');
+    // const btnCreateDb = document.getElementById('btnCreateDb');
+
+    // // for the trial in alternative way
+    // const Dataname=document.querySelector('#textDB')
+    // const Dateversion=document.querySelector('textversion')
+    // const Datesname=document.querySelector('#btnCreateDb')
+
+    // btnCreateDb.addEventListener('click', createDb);
+
+    // function createDb() {
+    //     const dbNameValue = Dbname.value;
+    //     const dbVersionValue = DbVersion.value; // Ensure it's a number
+
+    //     const request = indexedDB.open(dbNameValue, dbVersionValue);
+
+    //     // onupgradeneeded
+    //     request.onupgradeneeded = function(e) {
+    //         alert("Upgrade is called");
+    //     };
+
+    //     // onsuccess
+    //     request.onsuccess = function(e) {
+    //         alert("On success is called");
+    //     };
+
+    //     // onerror
+    //     request.onerror = function(e) {
+    //         alert("Error is called");
+    //     };
+    // }
+
